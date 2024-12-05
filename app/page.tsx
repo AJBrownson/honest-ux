f5c5b45a130ed903649354298306d683563cb0da
Anietie Brownson <anni.brownson@gmail.com>
2024-12-05 08:11:50 +0100

    feat: Add new code changes

--- app/page.tsx
+++ app/page.tsx
@@ -16,24 +16,24 @@ export default function Home() {
             <Image src={Logo} alt="Honest's Logo" className="w-8 h-8 lg:w-16 lg:h-16 object-contain" />
           </div>
 
-          <p className="text-[#A7AAB4] font-medium text-justify md:text-center text-xs md:text-base mt-14">
-            Hey, <span className="text-[#F9F9F9]">I’m Honest</span>, a{" "}
-            <span className="text-[#F9F9F9]">UI/UX</span> and{" "}
-            <span className="text-[#F9F9F9]">brand designer</span> who’s all
-            about making life <span className="text-[#F9F9F9]">easier</span> for
+          <p className="text-bodyText font-poppins font-medium text-justify md:text-center text-xs md:text-base mt-14">
+            Hey, <span className="text-titleText">I’m Honest</span>, a{" "}
+            <span className="text-titleText">UI/UX</span> and{" "}
+            <span className="text-titleText">brand designer</span> who’s all
+            about making life <span className="text-titleText">easier</span> for
             people using{" "}
-            <span className="text-[#F9F9F9]">digital products</span>. I design
+            <span className="text-titleText">digital products</span>. I design
             cool
-            <span className="text-[#F9F9F9]"> interfaces</span>,{" "}
-            <span className="text-[#F9F9F9]">smooth experiences</span>, and{" "}
-            <span className="text-[#F9F9F9]">
+            <span className="text-titleText"> interfaces</span>,{" "}
+            <span className="text-titleText">smooth experiences</span>, and{" "}
+            <span className="text-titleText">
               create illustrations, design systems
             </span>
-            , and <span className="text-[#F9F9F9]">brands</span> that stand out.
+            , and <span className="text-titleText">brands</span> that stand out.
             This is my own spot in the big
-            <span className="text-[#F9F9F9]"> design world</span>, where I turn{" "}
-            <span className="text-[#F9F9F9]">ideas</span> into something{" "}
-            <span className="text-[#F9F9F9]">real and useful</span>!
+            <span className="text-titleText"> design world</span>, where I turn{" "}
+            <span className="text-titleText">ideas</span> into something{" "}
+            <span className="text-titleText">real and useful</span>!
           </p>
         </div>
 
@@ -41,31 +41,31 @@ export default function Home() {
           <div className="md:bg-[#1A1B1E] max-w-[1200px] rounded-lg md:py-6 md:px-[72px] flex flex-col">
             <div className="flex space-x-3 mb-4 md:mb-10">
               <Image src={CarIcon} alt="" className="" />
-              <p>Pick Your Path, I’ll Take You There!</p>
+              <p className="text-titleText font-poppins font-medium">Pick Your Path, I’ll Take You There!</p>
             </div>
 
             <div className="flex gap-x-4 md:gap-x-8">
               <Link href="/about">
-                <div className="border border-[#232529] rounded-lg p-2 bg-[#232529]">
+                <div className="border border-stroke rounded-lg p-2 bg-bg">
                   <Image src={Portrait} alt="Honest's Portrait" className="lg:w-[242px] lg:h-[224px] object-contain " />
                   <div className="pt-4 pb-2 px-2">
-                    <p className="text-[#A7AAB4] text-[10px] md:text-xs">
+                    <p className="font-poppins text-bodyText text-[10px] md:text-xs">
                       About Honest
                     </p>
-                    <p className="font-semibold text-[#F9F9F9] text-xs md:text-base">
+                    <p className="font-poppins font-semibold text-titleText text-xs md:text-base">
                       My Portfolio
                     </p>
                   </div>
                 </div>
               </Link>
 
-              <div className="border border-[#232529] rounded-lg p-2 bg-[#232529]">
+              <div className="border border-stroke rounded-lg p-2 bg-bg">
                 <Image src={Shop} alt="Photo of a shopping bag" />
                 <div className="pt-4 pb-2 px-2">
-                  <p className="text-[#A7AAB4] text-[10px] md:text-xs">
+                  <p className="font-poppins text-bodyText text-[10px] md:text-xs">
                     Accessories
                   </p>
-                  <p className="font-semibold text-[#F9F9F9] text-xs md:text-base">
+                  <p className="font-poppins font-semibold text-titleText text-xs md:text-base">
                     Store
                   </p>
                 </div>
