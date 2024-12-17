547339bf5b773807107fcacc9646df836f15d6b2
Anietie Brownson <anni.brownson@gmail.com>
2024-12-17 19:05:58 +0100

    feat: Add carousel and testimonial code edits

--- app/components/Testimonials.tsx
+++ app/components/Testimonials.tsx
@@ -3,7 +3,6 @@ import Steve from "@/public/steve.svg";
 import Brown from "@/public/brown.svg";
 import Morgan from "@/public/justus.svg";
 
-import { Card, CardContent } from "@/app/components/ui/card";
 import {
   Carousel,
   CarouselContent,
@@ -76,6 +75,7 @@ export default function Testimonials() {
         <button className="p-2 bg-white rounded-full">→</button>
       </div>
     </div> */}
+
       {/* <div className="flex space-x-4 overflow-x-auto">
         <div className="bg-bg flex flex-col lg:flex-row w-full max-w-[515px] border border-stroke py-4 rounded-xl">
           <div className="w-[32%] lg:border-r-[1px] border-r-stroke flex flex-col items-center">
@@ -165,27 +165,48 @@ export default function Testimonials() {
         </div>
       </div> */}
 
-      <Carousel className="w-full max-w-sm font-poppins">
+      <Carousel className="w-full max-w-xl font-poppins">
         <CarouselContent className="-ml-1">
           {Array.from({ length: 3 }).map((_, index) => (
-            <CarouselItem
-              key={index}
-              // className="pl-1 md:basis-1/2 lg:basis-1/3"
-              className="pl-1 basis-[80%]"
-            >
-              <div className="p-1">
-                <Card>
-                  {/* <CardContent className="flex aspect-square items-center justify-center p-6"> */}
-                  <CardContent className="flex items-center justify-center">
-                    <span className="text-2xl font-semibold">{index + 1}</span>
-                  </CardContent>
-                </Card>
+            <CarouselItem key={index} className="pl-1 basis-[80%]">
+              <div className="p-1 flex">
+
+                <div className="bg-bg flex flex-col lg:flex-row w-full lg:w-[650px] border border-stroke py-4 rounded-xl px-4 lg:px-0">
+                  <div className="w-full lg:w-[32%] border-b-[1px] border-b-stroke lg:border-b-0 lg:border-r-[1px] border-r-stroke flex flex-col items-center">
+                    <Image
+                      src={Morgan}
+                      alt=""
+                      className="rounded-full w-8 h-8 lg:w-20 lg:h-20 mb-4 lg:mt-2"
+                    />
+                    <span className="flex flex-col items-center">
+                      <p className="text-titleText font-bold font-poppins text-xs md:text-sm">
+                        Mr Morgan
+                      </p>
+                      <p className="text-bodyText font-poppins text-[10px] md:text-xs mt-1">
+                        CEO Fitgeng
+                      </p>
+                    </span>
+                  </div>
+
+                  <div className="px-3 lg:px-6 w-full max-w-[325px]">
+                    <p className="font-poppins font-medium text-5xl lg:text-7xl lg:mt-8 text-titleText">
+                      “
+                    </p>
+                    <p className="font-poppins font-medium text-bodyText text-sm lg:text-[15px] max-w-[277px]">
+                      Working with Honest was a game-changer for our project.
+                      His creativity and attention to detail exceeded our
+                      expectations! I’m glad i met you Thank you.
+                    </p>
+                  </div>
+                </div>
               </div>
             </CarouselItem>
           ))}
         </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
+        <div className="hidden lg:flex justify-end space-x-3 pt-10">
+          <CarouselPrevious />
+          <CarouselNext />
+        </div>
       </Carousel>
     </>
   );
