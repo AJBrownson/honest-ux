1e55ff67d2f5171e32c213b959ac49b82de4e02a
Anietie Brownson <anni.brownson@gmail.com>
2025-01-09 12:06:38 +0100

    feat: Add hover effect to the cards

--- app/components/PersonalCards.tsx
+++ app/components/PersonalCards.tsx
@@ -24,7 +24,7 @@ export default function PersonalProjects() {
     <>
       <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-3 gap-4">
         {data.map((item, index) => (
-          <div key={index} className="w-full border border-stroke bg-bg rounded-xl p-2">
+          <div key={index} className="relative group w-full border border-stroke bg-bg rounded-xl p-2">
             <div className="bg-[#D1D1D2] w-full h-[224px] rounded-lg"></div>
             <div className="my-3 px-2">
               <p className="mb-1 font-poppins text-[10px] lg:text-xs text-bodyText">
@@ -34,6 +34,9 @@ export default function PersonalProjects() {
                 {item.name}
               </p>
             </div>
+            <div className="flex justify-center relative">
+                    <div className="absolute -bottom-[10px] p-[0.8px] w-[130px] bg-gradient-to-r from-[#2F3037] via-[#FFFFFF] to-[#2F3037] [background:linear-gradient(90deg,#2F3037 7%,#FFFFFF 53%,#2F3037 91%)] opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
+                  </div>
           </div>
         ))}
       </div>
